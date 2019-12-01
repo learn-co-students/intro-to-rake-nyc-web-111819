cda71304a74410e33fdb206e95cd3d9c20069a65
github username <github email address>
2019-11-30 22:18:45 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,5 @@
 require_relative "../config/environment.rb"
-
+require "pry"
 class Student
 
   # Remember, you can access your database connection anywhere in this class
@@ -52,3 +52,6 @@ class Student
   end
 
 end
+
+# Binding.pry 
+0
